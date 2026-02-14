6eec05becf1d8ef46bd14f06c6b3e22c117af275
manat_laoop <manat850112@gmail.com>
2026-02-14 16:57:05 +0700

    first commit

---
+++ src/components/empoyee_component.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>empoyee</div>
+</template>
+
+<script setup></script>
+<style></style>
---
+++ src/router/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home_component from "../components/home_component.vue";
+
+const router = [
+  {
+    path: "/",
+    name: "home",
+    component: Home_component,
+  },
+  {
+    path: "/",
+    name: "home",
+    component: Home_component,
+  },
+];
