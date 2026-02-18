005b0e2493f6c68c328b399b479232219ec660ff
manat_laoop <manat850112@gmail.com>
2026-02-18 19:40:28 +0700

    update tail

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,11 @@
 
 
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/banrai-school/',   // ⬅️ ชื่อ repo
-})
+  plugins: [vue(), tailwindcss()],
+  base: "/banrai-school/",
+});
